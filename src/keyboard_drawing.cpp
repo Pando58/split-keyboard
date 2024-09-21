2186bc422c6f80d953881021aa2ec69ed0b8cbcc
Enrique pando <enr.pando@gmail.com>
2024-09-20 22:06:58 -0600

    draw keys on both displays

--- src/keyboard_drawing.cpp
+++ src/keyboard_drawing.cpp
@@ -1,6 +1,6 @@
 #include "keyboard_drawing.h"
 
-int16_t get_position(uint8_t i) {
+int16_t get_position(uint8_t i, bool flip) {
 	int8_t x_initial;
 	int8_t y_initial;
 
@@ -17,19 +17,20 @@ int16_t get_position(uint8_t i) {
 		y_initial = 37 + ((i - 19) / 3 * 10) + (x_cell * 3);
 	}
 
+	if (flip) {
+		x_initial = 64 - 11 - x_initial;
+	}
+
 	int8_t x_rotated = -10 - y_initial;
 	int8_t y_rotated = x_initial;
 
-	int8_t x = x_rotated + 103;
-	int8_t y = y_rotated;
-
-	return ((int16_t)x << 8) | y;
+	return ((int16_t)x_rotated << 8) | y_rotated;
 }
 
-int8_t keyboard_drawing::get_position_x(uint8_t index) {
-	return get_position(index) >> 8;
+int8_t keyboard_drawing::get_position_x(uint8_t index, bool flip) {
+	return get_position(index, flip) >> 8;
 }
 
-int8_t keyboard_drawing::get_position_y(uint8_t index) {
-	return get_position(index) & 0xFF;
+int8_t keyboard_drawing::get_position_y(uint8_t index, bool flip) {
+	return get_position(index, flip) & 0xFF;
 }
--- src/scenes/keyboard_mode.cpp
+++ src/scenes/keyboard_mode.cpp
@@ -7,23 +7,37 @@ KeyboardMode::KeyboardMode(Adafruit_SSD1306 *display0, Adafruit_SSD1306 *display
 
 void KeyboardMode::enter() {
 	m_display0->clearDisplay();
+	m_display1->clearDisplay();
 
 	for (uint8_t i = 0; i < TOTAL_KEYS_ONE_SIDE; i++) {
-		int8_t x = keyboard_drawing::get_position_x(i);
+		int8_t x = keyboard_drawing::get_position_x(i) + 103;
 		int8_t y = keyboard_drawing::get_position_y(i);
 
 		m_display0->drawRect(x, y, 11, 11, SSD1306_WHITE);
 	}
 
+	for (uint8_t i = 0; i < TOTAL_KEYS_ONE_SIDE; i++) {
+		int8_t x = keyboard_drawing::get_position_x(i, true) + 103;
+		int8_t y = keyboard_drawing::get_position_y(i, true);
+
+		m_display1->drawRect(x, y, 11, 11, SSD1306_WHITE);
+	}
+
 	m_display0->display();
+	m_display1->display();
 }
 
 void KeyboardMode::loop() {}
 
 void KeyboardMode::onKeyChange(uint8_t index, bool pressed) {
-	int8_t x = keyboard_drawing::get_position_x(index);
-	int8_t y = keyboard_drawing::get_position_y(index);
+	bool left_side = index < 25;
+	index %= 25;
 
-	m_display0->fillRect(x + 3, y + 3, 5, 5, pressed ? SSD1306_WHITE : SSD1306_BLACK);
-	m_display0->display();
+	int8_t x = keyboard_drawing::get_position_x(index, !left_side) + 103;
+	int8_t y = keyboard_drawing::get_position_y(index, !left_side);
+
+	Adafruit_SSD1306 *display = left_side ? m_display0 : m_display1;
+
+	display->fillRect(x + 3, y + 3, 5, 5, pressed ? SSD1306_WHITE : SSD1306_BLACK);
+	display->display();
 }
